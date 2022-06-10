681c363ca956c854af33cde2caae65ae6caa263d
Ishaan0310 <ishaanpandey31@gmail.com>
2022-06-10 16:53:29 +0530

    thirfcommit
    thirdcommit

--- src/main/java/com/eaiesb/training/SummerInterns/InternsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.eaiesb.training.SummerInterns;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface InternsRepository extends MongoRepository <Interns, String> {
-
-}
